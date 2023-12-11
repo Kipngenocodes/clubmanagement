7721868f86f7358b407d36016b549c94dcbf0749
Kipngenocodes <kipngenoalex.c@gmail.com>
2023-12-11 13:03:54 -0700

    Login form setup

--- Clubwebsite/urls.py
+++ Clubwebsite/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home.html'),
+    path('', views.home, name='home'),
+    # path('Login/', views.login_student, name='login'),
+    # path('Logout/', views.logout_student, name='Logout'),
+
 
 ]
--- Clubwebsite/views.py
+++ Clubwebsite/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+
+def login_student(request):
+    pass
+
+
+def logout_student(request):
+    pass
